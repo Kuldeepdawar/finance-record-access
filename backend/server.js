b8ae0de9c405f38ffdd32b7ffb7886fd070b5034
KuldeepDawar <kuldeepdawar47@gmail.com>
2024-11-12 10:11:45 +1100

    change Server PORT and edit in .env file

--- backend/server.js
+++ backend/server.js
@@ -15,5 +15,5 @@ connectDB();
 
 app.use("/api/finance", financeRoutes);
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
