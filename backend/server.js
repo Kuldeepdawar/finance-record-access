f4235cdd75e803b9c3753ed7f02c110ddda8e899
KuldeepDawar <kuldeepdawar47@gmail.com>
2024-11-11 13:59:28 +1100

    server setup and mongoDB

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+// server setup
+
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./config/db");
+const financeRoutes = require("./routes/financeRoutes");
+
+const app = express();
+
+require("dotenv").config();
+app.use(cors());
+app.use(express.json());
+
+connectDB();
+
+app.use("/api/finance", financeRoutes);
+
+const PORT = process.env.PORT || 5001;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
